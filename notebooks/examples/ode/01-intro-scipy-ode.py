d6068b3dd25b3c108e825e67f9a18ca3aeb8af1d
Diego Volpatto <volpatto@lncc.br>
2026-06-04 19:04:05 -0300

    Add paired .py files to ipynb files

---
+++ notebooks/examples/ode/01-intro-scipy-ode.py
@@ -0,0 +1,4 @@
+# %% [markdown]
+# ## Introdução à solução de ODEs com a SciPy
+#
+# Aqui veremos como utilizar a SciPy para solucionar alguns problemas clássicos de ODE.
---
+++ notebooks/examples/pde/01-intro-py-pde.py
@@ -0,0 +1,4 @@
+# %% [markdown]
+# ## Introdução ao solucionamento de PDEs com `py-pde`
+#
+# Aqui veremos como utilizar a biblioteca [py-pde](https://py-pde.readthedocs.io/en/latest/) para solucionar equações diferenciais parciais com método de diferenças finitas em Python.
